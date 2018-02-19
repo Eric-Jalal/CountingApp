d6853d8473bef7feea076ebe558c4d9f4881d0f4
Amirhossein Hosseini <Blackworld.2035@gmail.com>
2018-02-19 10:55:42 +0400

    Added button and textView using Anko

--- app/src/main/java/com/example/teslaa/countingapp/MainActivity.kt
+++ app/src/main/java/com/example/teslaa/countingapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.teslaa.countingapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counter = 0
+        counterButton.setOnClickListener {
+            counter++
+            counterTextView.text = counter.toString()
+        }
     }
 }
